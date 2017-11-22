4810525ca1d28ee658a50a2a662f89fe837ae50d
Vinit Mohata <vinit.mohata@dev.spdpl.com>
2017-11-22 17:44:35 +0530

    change

--- models/company.js
+++ models/company.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
-var AddressSchema = new Schema({
+let AddressSchema = new Schema({
     name: {
         type: String,
         required: [true, 'name is Required.']
